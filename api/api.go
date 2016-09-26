eef2934448f4e95cb738c26426141b7d760e0a19
Kasparas Galdikas <kasparas.galdikas@rakuten.com>
2016-09-27 00:21:59 +0100

    WIP

--- api/api.go
+++ api/api.go
@@ -4,19 +4,37 @@ import (
 	"net/http"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
 )
 
 type Api struct {
+	Router *mux.Router
 }
 
 func NewApi() {
-	api := Api{}
+	api := &Api{
+		Router: NewRouter(),
+	}
 
 	api.Listen()
+}
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+
+	for _, route := range routes {
+		router.
+			Methods(route.Method).
+			Path(route.Path).
+			Name(route.Name).
+			Handler(route.Handler)
+	}
+
+	return router
 }
 
 func (a *Api) Listen() {
 	log.Info("Listening on 8080")
 
-	http.ListenAndServe(":8080", NewRouter())
+	http.ListenAndServe(":8080", a.Router)
 }
